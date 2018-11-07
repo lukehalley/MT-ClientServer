1f4bbe7242058943cdf8bcc57b2763fbb091b0e1
lukehalley <luke123halley@gmail.com>
2018-11-07 12:32:22 +0000

    removed old server and renamed new server

--- src/lhalley/Server.java
+++ src/lhalley/Server.java
@@ -12,34 +12,37 @@ import java.awt.*;
 
 import javax.swing.*;
 
-@SuppressWarnings("serial")
-public class Server extends JFrame {
-
-	// DB configurations
-	private final String userName = "root";
-	private final String password = "";
-	private final String serverName = "localhost";
-	private final int portNumber = 3306;
-	private final String dbName = "wit";
-	private final String studentTable = "students";
-
+public class ServerThread extends Thread {
+	
 	public static void main(String[] args) {
-		new Server();
+		Connect c = new Connect();
+		c.run();
 	}
 
-	public Server() {
+}
+
+@SuppressWarnings("serial")
+class BuildGUI extends JFrame {
+	
+	JTextArea serverStatus;
+	JTextPane clientTitle;
+	JLabel currentStudentNumberLabel;
+	JTextArea currentStudentNumber;
+	JLabel currentStudentNameLabel;
+	JTextArea currentStudentName;
+	
+	public BuildGUI() {
 
 		getContentPane().setLayout(null);
 		// Panel p to hold the label and text field
 		JPanel p = new JPanel();
-		p.setBounds(0, 0, 484, 0);
 		p.setLayout(new BorderLayout());
 		getContentPane().add(p);
 
-		JTextArea serverStatus = new JTextArea();
-		serverStatus.setBounds(10, 11, 370, 239);
-		serverStatus.setEditable(false);
-		getContentPane().add(serverStatus);
+		JTextArea serverStatusTemp = new JTextArea();
+		serverStatusTemp.setBounds(10, 11, 370, 239);
+		serverStatusTemp.setEditable(false);
+		getContentPane().add(serverStatusTemp);
 
 		JTextPane clientTitle = new JTextPane();
 		clientTitle.setText("WIT Student Login");
@@ -47,31 +50,126 @@ public class Server extends JFrame {
 		clientTitle.setBounds(390, 11, 284, 20);
 		getContentPane().add(clientTitle);
 
-		JLabel currentStudentNumberLabel = new JLabel("Student ID");
+		JLabel currentStudentNumberLabel = new JLabel("Student Number");
 		currentStudentNumberLabel.setBounds(390, 42, 284, 20);
 		getContentPane().add(currentStudentNumberLabel);
-
-		JTextPane currentStudentNumber = new JTextPane();
-		currentStudentNumber.setEditable(false);
-		currentStudentNumber.setBounds(390, 73, 284, 20);
-		getContentPane().add(currentStudentNumber);
+		
+		JTextPane currentStudentNumberTemp = new JTextPane();
+		currentStudentNumberTemp.setEditable(false);
+		currentStudentNumberTemp.setBounds(390, 73, 284, 20);
+		getContentPane().add(currentStudentNumberTemp);
 
 		JLabel currentStudentNameLabel = new JLabel("Student Name");
 		currentStudentNameLabel.setBounds(390, 104, 284, 20);
 		getContentPane().add(currentStudentNameLabel);
 
-		JTextPane currentStudentName = new JTextPane();
-		currentStudentName.setEditable(false);
-		currentStudentName.setBounds(390, 135, 284, 20);
-		getContentPane().add(currentStudentName);
+		JTextPane currentStudentNameTemp = new JTextPane();
+		currentStudentNameTemp.setEditable(false);
+		currentStudentNameTemp.setBounds(390, 135, 284, 20);
+		getContentPane().add(currentStudentNameTemp);
 
 		setTitle("Server");
+		setBackground(Color.WHITE);  
 		setSize(700, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		setResizable(false);
+		getContentPane().repaint();
 
+	}
+
+	public JTextArea getServerStatus() {
+		return serverStatus;
+	}
+
+	public void setServerStatus(JTextArea serverStat) {
+		this.serverStatus = serverStat;
+		getContentPane().add(serverStat);
+		getContentPane().repaint();
+	}
+
+	public JTextPane getClientTitle() {
+		return clientTitle;
+	}
+
+	public void setClientTitle(JTextPane clientTitle) {
+		this.clientTitle = clientTitle;
+		getContentPane().add(clientTitle);
+		getContentPane().repaint();
+	}
+
+	public JLabel getCurrentStudentNumberLabel() {
+		return currentStudentNumberLabel;
+	}
+
+	public void setCurrentStudentNumberLabel(JLabel currentStudentNumberLabel) {
+		this.currentStudentNumberLabel = currentStudentNumberLabel;
+		getContentPane().add(currentStudentNumberLabel);
+		getContentPane().repaint();
+	}
+
+	public JTextArea getCurrentStudentNumber() {
+		return currentStudentNumber;
+	}
+
+	public void setCurrentStudentNumber(JTextArea currentStudentNumber) {
+		this.currentStudentNumber = currentStudentNumber;
+		getContentPane().add(currentStudentNumber);
+		getContentPane().repaint();
+	}
+
+	public JLabel getCurrentStudentNameLabel() {
+		return currentStudentNameLabel;
+	}
+
+	public void setCurrentStudentNameLabel(JLabel currentStudentNameLabel) {
+		this.currentStudentNameLabel = currentStudentNameLabel;
+		getContentPane().add(currentStudentNameLabel);
+		getContentPane().repaint();
+	}
+
+	public JTextArea getCurrentStudentName() {
+		return currentStudentName;
+	}
+
+	public void setCurrentStudentName(JTextArea currentStudentName) {
+		this.currentStudentName = currentStudentName;
+		getContentPane().add(currentStudentName);
+		getContentPane().repaint();
+	}
+	
+	
+}
+
+@SuppressWarnings("serial")
+class Connect extends JPanel {
+
+	public final String userName = "root";
+	public final String password = "";
+	public final String serverName = "localhost";
+	public final int portNumber = 3306;
+	public final String dbName = "wit";
+	public String studentTable = "students";
+
+	// Function which is used to get a connection to the database
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		Properties connectionProps = new Properties();
+		// Setting credentials
+		connectionProps.put("user", this.userName);
+		connectionProps.put("password", this.password);
+		// Getting the connection using jdbc
+		conn = DriverManager.getConnection("jdbc:mysql://" + this.serverName + ":" + this.portNumber + "/" + this.dbName
+				+ "?verifyServerCertificate=false&useSSL=true", connectionProps);
+		return conn;
+	}
+
+	@SuppressWarnings("unused")
+	public void run() {
 		// Loading all the student id's in
+
 		try {
+			BuildGUI panel = new BuildGUI();
 			// Init of connection object
 			Connection dbConnection = null;
 			// Init of statement object
@@ -102,16 +200,30 @@ public class Server extends JFrame {
 				// Create data input and output streams
 				DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
 				DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-
+				
+				JTextArea serverStatus = new JTextArea();
+				serverStatus.setBounds(10, 11, 370, 239);
+				serverStatus.setEditable(false);
+				panel.setServerStatus(serverStatus);
+				
+				JTextArea currentStudentNumber = new JTextArea();
+				currentStudentNumber.setEditable(false);
+				currentStudentNumber.setBounds(390, 73, 284, 20);
+				panel.setCurrentStudentNumber(currentStudentNumber);
+				
+				JTextArea currentStudentName = new JTextArea();
+				currentStudentName.setEditable(false);
+				currentStudentName.setBounds(390, 135, 284, 20);
+				panel.setCurrentStudentName(currentStudentName);
+				
 				while (true) {
+
 					// Receive radius from the client
 					int recievedStudentNum = inputFromClient.readInt();
 
 					String sn = Integer.toString(recievedStudentNum);
 
 					String getUserByStNum = "SELECT * FROM " + studentTable + " WHERE STUD_ID = " + sn;
-
-					System.out.println(getUserByStNum);
 
 					ResultSet r = statement.executeQuery(getUserByStNum);
 					r.next();
@@ -123,14 +235,21 @@ public class Server extends JFrame {
 
 					// Compute area
 					boolean loginStatus;
-					serverStatus.append("Student number received from client: " + recievedStudentNum + '\n');
+					
+					JTextArea serverStat = panel.getServerStatus();
+					JTextArea currStudName = panel.getCurrentStudentName();
+					JTextArea currStudNum = panel.getCurrentStudentNumber();
+					
+					serverStat.append("Student number received from client: " + recievedStudentNum + '\n');
 
 					if (ids.contains(sn)) {
 						loginStatus = true;
-						serverStatus.append(
+						serverStat.append(
 								"Student '" + customerNAME + "' is now logged in and connected to the Server " + '\n');
-						currentStudentNumber.setText(customerSTUDID);
-						currentStudentName.setText(customerNAME);
+						currStudNum.append(customerSTUDID);
+						System.out.print(currStudNum.getText());
+						currStudName.append(customerNAME);
+						System.out.print(currStudName.getText());
 						try {
 							outputToClient.writeBoolean(loginStatus);
 							outputToClient.writeUTF(customerNAME);
@@ -139,10 +258,14 @@ public class Server extends JFrame {
 						}
 					} else {
 						loginStatus = false;
-						serverStatus.append("Student does NOT exsist in database, login unsucessfull" + '\n');
+						serverStat.append("Student does NOT exsist in database, login unsucessfull" + '\n');
+						serverSocket.close();
 					}
 
 				}
+				
+				
+				
 			} catch (IOException ex) {
 				System.err.println(ex);
 			}
@@ -159,16 +282,4 @@ public class Server extends JFrame {
 
 	}
 
-	// Function which is used to get a connection to the database
-	public Connection getConnection() throws SQLException {
-		Connection conn = null;
-		Properties connectionProps = new Properties();
-		// Setting credentials
-		connectionProps.put("user", this.userName);
-		connectionProps.put("password", this.password);
-		// Getting the connection using jdbc
-		conn = DriverManager.getConnection("jdbc:mysql://" + this.serverName + ":" + this.portNumber + "/" + this.dbName
-				+ "?verifyServerCertificate=false&useSSL=true", connectionProps);
-		return conn;
-	}
 }
